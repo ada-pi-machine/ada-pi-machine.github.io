c95d49dcbd6a5303f578cbd57f2417782e10f06b
rekuenkdr <42606743+rekuenkdr@users.noreply.github.com>
2022-07-15 19:48:16 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/docs/' />;
+  return <Redirect to='https://armada-alliance.com/docs/' />;
 }
